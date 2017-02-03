756b8c58054ecacc79a6da247c26efb193a4b0b1
yangtaochina <yangtaochina@163.com>
2017-02-03 10:56:41 +0800

    dev get set 方法生成

--- Git2/src/com/test/Main.java
+++ Git2/src/com/test/Main.java
@@ -3,4 +3,17 @@ package com.test;
 public class Main {
 	private int age;
 	private String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
